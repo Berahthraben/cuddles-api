c163e5362b08c03ea8da8bbec964ebb960805200
Ricardo Montania Prado de Campos <ricardompcampos@gmail.com>
2019-10-11 11:14:07 -0300

    Added CORS support

--- src/dependencies.php
+++ src/dependencies.php
@@ -10,3 +10,18 @@ $container['logger'] = function ($c) {
     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
     return $logger;
 };
+
+// Enable CORS
+// http://www.slimframework.com/docs/v3/cookbook/enable-cors.html
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://mysite')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
